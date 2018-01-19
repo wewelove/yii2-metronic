41794e45af18f5d0f36e34b0950041739341c68e
WangHao <wewelove88@gmail.com>
2018-01-19 17:32:36 +0800

    新增layout1模板

---
+++ layouts/layout1/main.php
@@ -0,0 +1,54 @@
+<?php
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use iamok\metronic\Metronic;
+use iamok\metronic\helpers\Layout;
+
+$asset = Metronic::registerThemeAsset($this);
+
+$directoryAsset = Yii::$app->assetManager->getPublishedUrl($asset->sourcePath);
+?>
+<?php $this->beginPage() ?>
+<!--[if IE 8]> <html lang="<?= Yii::$app->language ?>" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="<?= Yii::$app->language ?>" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<!--<![endif]-->
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <title><?= Html::encode($this->title) ?></title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <meta content="" name="description">
+    <meta content="" name="author">
+
+    <?= Html::csrfMetaTags() ?>
+    <?php $this->head() ?>
+</head>
+<body <?= Layout::getHtmlOptions('body', [], true) ?> >
+<?php $this->beginBody() ?>
+
+    <div class="page-wrapper">
+
+        <?= $this->render('parts/header.php', ['directoryAsset' => $directoryAsset]) ?>
+
+        <div class="page-container">
+
+            <?= $this->render('parts/sidebar.php', ['directoryAsset' => $directoryAsset]) ?>
+
+            <?= $this->render('parts/content.php', ['content' => $content, 'directoryAsset' => $directoryAsset]) ?>
+
+            <?= $this->render('parts/chatbar.php', ['directoryAsset' => $directoryAsset]) ?>
+
+        </div>
+
+        <?= $this->render('parts/footer.php', ['directoryAsset' => $directoryAsset]) ?>
+    </div>
+    
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
---
+++ layouts/layout1/parts/content.php
@@ -0,0 +1,11 @@
+<?php
+use yii\helpers\Html;
+use iamok\metronic\widgets\Breadcrumbs;
+use iamok\metronic\widgets\Button;
+?>
+
+<div class="page-content-wrapper">
+    <div class="page-content">
+        <?= $content ?>
+    </div>
+</div>
